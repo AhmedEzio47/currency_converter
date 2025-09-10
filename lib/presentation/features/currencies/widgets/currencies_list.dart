e0912b4c28ae962d2e06ee84d24e12f0717118d6
Ahmed Nabil <ahmednab93@gmail.com>
2025-09-11 00:25:17 +0300

    feat: add a staggering animation for the currency list

--- lib/presentation/features/currencies/widgets/currencies_list.dart
+++ lib/presentation/features/currencies/widgets/currencies_list.dart
@@ -1,5 +1,6 @@
 import 'package:currency_converter/data/models/currency_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 import 'currency_item.dart';
 
@@ -14,7 +15,11 @@ class CurrenciesList extends StatelessWidget {
       separatorBuilder: (_, _) => const SizedBox(height: 5),
       itemBuilder: (_, index) => Padding(
         padding: const EdgeInsets.symmetric(horizontal: 5),
-        child: CurrencyItem(currency: currencies[index]),
+        child: CurrencyItem(currency: currencies[index]).animate().slide(
+          begin: Offset(1, 0),
+          duration: 300.ms,
+          delay: Duration(milliseconds: index * 100),
+        ),
       ),
       itemCount: currencies.length,
     );
